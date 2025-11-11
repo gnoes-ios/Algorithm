d7698e8933452a6ece5933ea7434090b7b963698
gnoes-ios <159489050+gnoes-ios@users.noreply.github.com>
2025-11-11 15:02:05 +0900

    [Silver IV] Title: 수 찾기, Time: 100 ms, Memory: 84724 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.swift
+++ 백준/Silver/1920. 수 찾기/수 찾기.swift
@@ -1,13 +1,46 @@
-let n = Int(readLine()!)!
-let nSet = Set(readLine()!.split(separator: " ").map { Int($0)! })
+import Foundation
 
-let m = Int(readLine()!)!
-let mArr = readLine()!.split(separator: " ").map { Int($0)! }
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
 
-for num in mArr {
-    if nSet.contains(num) {
-        print(1)
-    } else {
-        print(0)
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fileHandle.readToEnd()!) + [UInt8(0)]
     }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10 || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readInts(count: Int) -> [Int] {
+        return (0 ..< count).map { _ in readInt() }
+    }
+}
+
+let file = FileIO()
+
+let N = file.readInt()
+let A = Set(file.readInts(count: N))
+let M = file.readInt()
+
+for _ in 0..<M {
+    let target = file.readInt()
+    A.contains(target) ? print(1) : print(0)
 }
